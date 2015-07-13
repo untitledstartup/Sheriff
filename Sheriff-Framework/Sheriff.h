4a5209b319a6b6343ed0ae1af6b0c94342ae84c6
Katsuma Ito <katsumai@gmail.com>
2015-07-13 09:19:03 +0800

    Adds framework support

---
+++ Sheriff-Framework/Sheriff.h
@@ -0,0 +1,17 @@
+//
+//  Sheriff-Framework.h
+//  Sheriff-Framework
+//
+//  Created by ka2n on 2015/07/13.
+//  Copyright (c) 2015年 Michael Amaral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sheriff-Framework.
+FOUNDATION_EXPORT double SheriffVersionNumber;
+
+//! Project version string for Sheriff-Framework.
+FOUNDATION_EXPORT const unsigned char SheriffVersionString[];
+
+#import <Sheriff/GIBadgeView.h>
